19a9db0088f900f0170309b70299e9958ded6ba2
Mia <vrevenkoen@gmail.com>
2024-01-27 15:51:50 +0100

    Add Room Gizmos

--- Assets/enemyscripts/Room.cs
+++ Assets/enemyscripts/Room.cs
@@ -12,12 +12,37 @@ public class Room : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, 1);
+        Gizmos.color = Color.green;
+        foreach (Room r in topRooms)
+        {
+            DrawArrow.ForGizmo(transform.position, Direction(transform.position, r.transform.position)*2);
+        }
+        foreach (Room r in bottomRooms)
+        {
+            DrawArrow.ForGizmo(transform.position, Direction(transform.position, r.transform.position)*2);
+        }
+        foreach (Room r in sideRooms)
+        {
+            DrawArrow.ForGizmo(transform.position, Direction(transform.position, r.transform.position)*2);
+        }
+    }
+    Vector3 Direction(Vector3 from, Vector3 to)
+    {
+        return (to - from).normalized;
     }
 }
