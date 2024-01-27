4ca65f702648d54940057eafcad77272fe841d71
Jorn2901 <j.uffing@students.uu.nl>
2024-01-27 10:17:24 +0100

    commit before pull

--- Assets/enemyscripts/EnemyBehaviour.cs
+++ Assets/enemyscripts/EnemyBehaviour.cs
@@ -30,6 +30,7 @@ public class EnemyBehaviour : MonoBehaviour
 
     void Update()
     {
+        print(Vector3.Angle(transform.position, new Vector3(0,0,0)));
         if (currentOxygen <= 0)
         {
             //go to the upmost top
@@ -42,13 +43,7 @@ public class EnemyBehaviour : MonoBehaviour
             timerChoice += Time.deltaTime;
 
             if (moving)
-            {
-                if (Vector3.Distance(transform.position, currentroom.transform.position) < radius)
-                {
-                    transform.position += transform.up * calmSpeed;
-                    // uhhhhh this is not correct
-                }
-            }
+                Movement();
 
             if (timerChoice > targetTimeChoice)
             {
@@ -56,59 +51,22 @@ public class EnemyBehaviour : MonoBehaviour
                 
                 int choice = Random.Range(0, 100);
 
-                float randomTime = Random.Range(0f, 2f);
-                
                 if (choice > currentOxygen)
                 {
                     //move a room up
                     if (currentroom.topRooms.Count > 0)
                     {
-                        print("moving up");
+                        MoveToRoom(currentroom.topRooms[Random.Range(0, currentroom.topRooms.Count - 1)]);
                     }
                 }
                 else
                 {
-                    switch (choice)
-                    {
-                        case < 50:
-                            //move in current room
-                            transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
-                            moving = true;
-                            print("moving in current room");
-                            break;
-                        case < 70:
-                            //move to a side room
-                            if (currentroom.sideRooms.Count > 0)
-                            {
-                                MoveToRoom(currentroom.sideRooms[Random.Range(0, currentroom.sideRooms.Count - 1)]);
-                                print("moving to a side room");
-                            }
-
-                            break;
-                        case < 90:
-                            //move down
-                            if (currentroom.bottomRooms.Count > 0)
-                            {
-                                MoveToRoom(currentroom.bottomRooms[Random.Range(0, currentroom.bottomRooms.Count - 1)]);
-                                print("moving to bottom");
-                            }
-                            break;
-                        default:
-                            //move up
-                            if (currentroom.topRooms.Count > 0)
-                            {
-                                MoveToRoom(currentroom.topRooms[Random.Range(0, currentroom.topRooms.Count - 1)]);
-                                print("moving to a top");
-                            }
-
-                            break;
-
-                    }
+                    MakeADescicion(choice);
                 }
 
                 timerChoice = 0;
                 //generate a random time 
-                targetTimeChoice = randomTime;
+                targetTimeChoice = Random.Range(0f, 2f);
             }
         }
         else
@@ -133,5 +91,52 @@ public class EnemyBehaviour : MonoBehaviour
     public void TrySeeFunny(float f, Vector3 position)
     {
         throw new System.NotImplementedException();
+        if (Vector3.Angle(transform.right, position) > 10)
+        {
+            
+        }
+    }
+
+    void Movement()
+    {
+        if (Vector3.Distance(transform.position, currentroom.transform.position) < radius)
+        {
+            transform.position += transform.up * calmSpeed;
+        }
+    }
+
+    void MakeADescicion(int choice)
+    {
+        switch (choice)
+        {
+            case < 50:
+                //move in current room
+                transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+                moving = true;
+                break;
+            case < 70:
+                //move to a side room
+                if (currentroom.sideRooms.Count > 0)
+                {
+                    MoveToRoom(currentroom.sideRooms[Random.Range(0, currentroom.sideRooms.Count - 1)]);
+                }
+
+                break;
+            case < 90:
+                //move down
+                if (currentroom.bottomRooms.Count > 0)
+                {
+                    MoveToRoom(currentroom.bottomRooms[Random.Range(0, currentroom.bottomRooms.Count - 1)]);
+                }
+                break;
+            default:
+                //move up
+                if (currentroom.topRooms.Count > 0)
+                {
+                    MoveToRoom(currentroom.topRooms[Random.Range(0, currentroom.topRooms.Count - 1)]);
+                }
+                break;
+
+        }
     }
 }
