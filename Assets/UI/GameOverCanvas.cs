cbde3f0ffbca219bf056ea3ac4fdcbb1d53beaa0
Anumberix <99255403+Anumberix@users.noreply.github.com>
2024-01-27 15:51:10 +0100

    add diver count text
    canvas in prefabs

--- Assets/UI/GameOverCanvas.cs
+++ Assets/UI/GameOverCanvas.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameOverCanvas : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private Button restartButton;
+    [SerializeField] private TextMeshProUGUI diverCountText;
+
+    [SerializeField] private int diverCount = 6;
     // Start is called before the first frame update
     IEnumerator Start()
     {
         yield return null;
         GameManager.Instance.gameOverEvent.AddListener(EnableGameOverScreen);
+        GameManager.Instance.diverExitEvent.AddListener(UpdateDiverCount);
         restartButton.onClick.AddListener(Restart);
     }
 
@@ -24,6 +29,12 @@ public class GameOverCanvas : MonoBehaviour
     private void EnableGameOverScreen()
     {
         gameOverScreen.SetActive(true);
+    }
+
+    private void UpdateDiverCount()
+    {
+        diverCount -= 1;
+        diverCountText.text = "Divers left: " + diverCount;
     }
 
     private void Restart()
